3eac300e8a14fe927752752d21c16ee31fa32d1b
Varahiil <[200~^[[200~>
2023-08-05 14:50:25 +0300

    New messages

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println();
+        System.out.println("New Life");
+        System.out.println("New Life");
+        System.out.println("New Life");
     }
 }
