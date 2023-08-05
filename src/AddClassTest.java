ad94bd1e524ff6873faefed83764d573519a795b
Varahiil <[200~^[[200~>
2023-08-05 15:53:18 +0300

    String

--- src/AddClassTest.java
+++ src/AddClassTest.java
@@ -1,8 +1,12 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class AddClassTest {
     int age = 18;
     int age = 18;
     int age = 183;
     int age = 1128;
     int age = 228;
+
+    String perfomance = "Cool";
 
 }
